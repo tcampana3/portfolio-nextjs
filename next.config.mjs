cd12c9e63f05f9c97495d7bfcd0b059db6b6a767
tcampana3 <tcampana@iastate.edu>
2025-09-04 17:39:56 -0500

    Changes to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',              // static export -> creates /out
+    images: { unoptimized: true }, // Next/Image off (Pages can't optimize)
+    trailingSlash: true,           // friendlier with Pages
+  };
+  export default nextConfig;
+  
